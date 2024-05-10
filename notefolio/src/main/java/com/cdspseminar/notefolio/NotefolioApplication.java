80c3dac34e7358fa13dc7b9e2c9cdf3830e0b878
gardening-y <yj4185@duksung.ac.kr>
2024-05-11 04:30:42 +0900

    [init] Notefolio 프로젝트 생성

---
+++ notefolio/src/main/java/com/cdspseminar/notefolio/NotefolioApplication.java
@@ -0,0 +1,13 @@
+package com.cdspseminar.notefolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotefolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotefolioApplication.class, args);
+	}
+
+}
