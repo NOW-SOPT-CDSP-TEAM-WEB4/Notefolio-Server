d0b5d7d8c4c8b80ef1ffa94fd61ac8b59b5fe044
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-05-16 16:01:28 +0900

    feat : program entity 추가

---
+++ notefolio/src/main/java/com/cdspseminar/notefolio/domain/Program.java
@@ -0,0 +1,33 @@
+package com.cdspseminar.notefolio.domain;
+
+import com.cdspseminar.notefolio.domain.common.BaseTimeEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Program extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private String date;
+
+    private boolean online;
+
+    private Program(String title, String content, String date, boolean online){
+        this.title = title;
+        this.content = content;
+        this.date = date;
+        this.online = online;
+    }
+}
