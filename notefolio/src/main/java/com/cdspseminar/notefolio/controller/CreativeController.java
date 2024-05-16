dda9dea839717856c6abb0bbc15a3fa4e6440a1b
gardening-y <yj4185@duksung.ac.kr>
2024-05-17 04:05:18 +0900

    [feat] creative 조회 controller 구현 (#5)

---
+++ notefolio/src/main/java/com/cdspseminar/notefolio/controller/CreativeController.java
@@ -0,0 +1,33 @@
+package com.cdspseminar.notefolio.controller;
+
+import com.cdspseminar.notefolio.common.ApiResponse;
+import com.cdspseminar.notefolio.common.SuccessStatus;
+import com.cdspseminar.notefolio.dto.response.CreativesGetResponse;
+import com.cdspseminar.notefolio.service.CreativeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/creatives")
+@RequiredArgsConstructor
+@RestController
+public class CreativeController {
+    private final CreativeService creativeService;
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<?>> getCreatives() {
+        final CreativesGetResponse response = creativeService.getCreatives();
+        return ApiResponse.success(SuccessStatus.OK, response);
+    }
+
+//    createAt, updateAt 잘되는지 테스트용으로 그냥 만들어봤어요 잘됩니다.
+    @PostMapping
+    public ResponseEntity<ApiResponse<?>> createCreatives() {
+
+        creativeService.createCreative();
+        return ApiResponse.success(SuccessStatus.CREATED);
+    }
+}
