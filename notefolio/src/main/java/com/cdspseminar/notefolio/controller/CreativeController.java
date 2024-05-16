2bd5751673a8b226914095ada7b2168c81f64124
gardening-y <yj4185@duksung.ac.kr>
2024-05-17 04:53:46 +0900

    [feat] creative 검색 controller 구현 (#6)

--- notefolio/src/main/java/com/cdspseminar/notefolio/controller/CreativeController.java
+++ notefolio/src/main/java/com/cdspseminar/notefolio/controller/CreativeController.java
@@ -6,10 +6,7 @@ import com.cdspseminar.notefolio.dto.response.CreativesGetResponse;
 import com.cdspseminar.notefolio.service.CreativeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/creatives")
 @RequiredArgsConstructor
@@ -23,10 +20,15 @@ public class CreativeController {
         return ApiResponse.success(SuccessStatus.OK, response);
     }
 
+    @GetMapping("/search")
+    public ResponseEntity<ApiResponse<?>> searchCreatives(@RequestParam String word) {
+        final CreativesGetResponse response = creativeService.searchCreatives(word);
+        return ApiResponse.success(SuccessStatus.OK, response);
+    }
+
 //    createAt, updateAt 잘되는지 테스트용으로 그냥 만들어봤어요 잘됩니다.
     @PostMapping
     public ResponseEntity<ApiResponse<?>> createCreatives() {
-
         creativeService.createCreative();
         return ApiResponse.success(SuccessStatus.CREATED);
     }
