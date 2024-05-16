956854dc8cb5a9d34cea1878cd19844e2f64c9f7
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-05-16 16:02:35 +0900

    feat : ProgramController 구현

---
+++ notefolio/src/main/java/com/cdspseminar/notefolio/controller/ProgramController.java
@@ -0,0 +1,24 @@
+package com.cdspseminar.notefolio.controller;
+
+import com.cdspseminar.notefolio.common.ApiResponse;
+import com.cdspseminar.notefolio.common.SuccessStatus;
+import com.cdspseminar.notefolio.dto.response.ProgramsFindResponse;
+import com.cdspseminar.notefolio.service.ProgramService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/program")
+@RequiredArgsConstructor
+public class ProgramController {
+    private final ProgramService programService;
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<?>> findPrograms(){
+        final ProgramsFindResponse response = programService.getPrograms();
+        return ApiResponse.success(SuccessStatus.OK,response);
+    }
+}
