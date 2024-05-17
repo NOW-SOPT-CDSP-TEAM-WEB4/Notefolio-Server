b6c50a05d1f68456a9022797edb51e01a030e3ef
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-05-17 18:39:34 +0900

    [feat] 좋아요 추가 HeartService 구현

--- notefolio/src/main/java/com/cdspseminar/notefolio/service/HeartService.java
+++ notefolio/src/main/java/com/cdspseminar/notefolio/service/HeartService.java
@@ -1,8 +1,13 @@
 package com.cdspseminar.notefolio.service;
 
+import com.cdspseminar.notefolio.common.exception.BusinessException;
+import com.cdspseminar.notefolio.common.exception.ErrorStatus;
+import com.cdspseminar.notefolio.domain.Creative;
+import com.cdspseminar.notefolio.domain.Creator;
+import com.cdspseminar.notefolio.domain.Heart;
 import com.cdspseminar.notefolio.dto.request.HeartCreateRequest;
+import com.cdspseminar.notefolio.repository.CreativeRepository;
 import com.cdspseminar.notefolio.repository.HeartRepository;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,9 +16,19 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class HeartService {
     private final HeartRepository heartRepository;
-
+    private final CreativeRepository creativeRepository;
     @Transactional
-    public void createHeart(HeartCreateRequest heartCreateRequest){
-
+    public void createHeart(Long creatorId, HeartCreateRequest heartCreateRequest){
+        Creator creator = new Creator(creatorId, "김솝트");
+        Creative creative = creativeRepository.findById(heartCreateRequest.creativeId())
+                .orElseThrow(
+                        () -> new BusinessException(ErrorStatus.CREATIVE_NOT_FOUND)
+                );
+        Heart heart = Heart.builder()
+                .creator(creator)
+                .creative(creative)
+                .build();
+        heartRepository.save(heart);
+        creative.increaseNumLike();
     }
 }
