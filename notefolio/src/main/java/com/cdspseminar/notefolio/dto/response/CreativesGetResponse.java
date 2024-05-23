96a584ce925ba6c80fccfed63bbbb1641bc894cc
gardening-y <yj4185@duksung.ac.kr>
2024-05-24 03:25:00 +0900

    [feat] 크리에이터 dto 스펙 변경 구현 (#22)

--- notefolio/src/main/java/com/cdspseminar/notefolio/dto/response/CreativesGetResponse.java
+++ notefolio/src/main/java/com/cdspseminar/notefolio/dto/response/CreativesGetResponse.java
@@ -1,6 +1,7 @@
 package com.cdspseminar.notefolio.dto.response;
 
 import com.cdspseminar.notefolio.domain.Creative;
+import com.cdspseminar.notefolio.repository.HeartRepository;
 import lombok.AccessLevel;
 import lombok.Builder;
 
@@ -10,14 +11,19 @@ import java.util.List;
 public record CreativesGetResponse(
     List<CreativeGetResponse> creatives
 ) {
-    public static CreativesGetResponse of(List<Creative> creatives) {
+    public static CreativesGetResponse of(List<Creative> creatives, HeartRepository heartRepository) {
         return CreativesGetResponse.builder().creatives(creatives.stream()
-               .map(creative -> CreativeGetResponse.builder()
-                       .name(creative.getCreator().getName())
-                       .creativeId(creative.getId())
-                       .view(creative.getView())
-                       .like(creative.getNumLike())
-                       .build())
+               .map(creative -> {
+                   boolean isLike = heartRepository.findByCreatorIdAndCreativeId(1L, creative.getId()).isPresent();
+                       return CreativeGetResponse.builder()
+                               .name(creative.getCreator().getName())
+                               .creativeId(creative.getId())
+                               .view(creative.getView())
+                               .like(creative.getNumLike())
+                               .isLike(isLike)
+                               .build();
+                   }
+               )
                .toList())
                .build();
     }
