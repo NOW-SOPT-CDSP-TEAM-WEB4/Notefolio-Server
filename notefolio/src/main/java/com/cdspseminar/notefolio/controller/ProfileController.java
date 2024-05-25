3f649e53a06094536ee894c5bbd36e44a3232c4f
gardening-y <yj4185@duksung.ac.kr>
2024-05-25 14:42:52 +0900

    [feat] cicd 연습용 커밋 (#22)

---
+++ notefolio/src/main/java/com/cdspseminar/notefolio/controller/ProfileController.java
@@ -0,0 +1,25 @@
+package com.cdspseminar.notefolio.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/profile")
+public class ProfileController {
+
+    private final Environment env;
+    private static final String NULL = "";
+
+    @GetMapping
+    public String getProfile() {
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse(NULL);
+    }
+}
